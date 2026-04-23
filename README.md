f85cb9113100a74baaf053d81cbd3092cf94cec8
BenjaCRR <bramirezreyna@alumno.huergo.edu.ar>
2026-04-22 23:52:34 -0300

    Mejora interfaz
    Hice que se vea mas lindo. añadí una función clave (uso de rueda para zoom)

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # TP_lab_algoritmo
 
+Estos son las librerías que deberás instalar para poder correr correctamente el programa.
+
+import pandas	,En consola: pip install pandas,	    Manejo de tablas de datos.
+import plotly	,En consola: pip install plotly,	    Crear el mapa interactivo.
+import requests	,En consola: pip install requests,	    Descargar los datos de internet.
+import webview	,En consola: pip install pywebview,     Crear la ventana de aplicación.
+
+bitácora:
+8-9
+15-16
+22-23
--- TP_TERREMOTOS.py
+++ TP_TERREMOTOS.py
@@ -28,16 +28,36 @@ fig = px.scatter_mapbox(
     df, lat="Latitud", lon="Longitud", size="Magnitud",
     color="Magnitud", hover_name="Lugar", zoom=1,
     mapbox_style="carto-darkmatter",
-    title="Terremotos - Últimas 24 Horas"
+    title="Terremotos - Últimas 24 Horas",
+    color_continuous_scale="OrRd"
+)
+
+fig.update_layout(
+    margin={"r":0,"t":50,"l":0,"b":50}, # t:40 deja espacio solo para el título
+    paper_bgcolor="black", # Fondo de la "ventana" en negro
+    plot_bgcolor="black",
+    font_color="white",
+       title={
+        'text': "MONITOR DE ACTIVIDAD SÍSMICA GLOBAL",
+        'y':0.95, 'x':0.5, 'xanchor': 'center', 'yanchor': 'top'
+          },
+          annotations=[{
+        'text': "Usa la rueda del ratón para hacer Zoom • Los puntos más grandes y rojos indican mayor magnitud",
+        'showarrow': False,
+        'xref': "paper", 'yref': "paper",
+        'x': 0.5, 'y': -0.1, # Posición abajo del mapa
+        'font': {'size': 12, 'color': "gray"}
+    }]
 )
 
 # 3. Guardar el mapa como un archivo HTML temporal
 archivo_html = "Terremotos.html"
-fig.write_html(archivo_html)
+fig.write_html(archivo_html, config={'displayModeBar': False,'scrollZoom': True })
+
 
 # 4. Abrir el archivo en una ventana de escritorio independiente
 # Esto crea una ventana real en VS Code, no abre el navegador
-webview.create_window('Visor de Terremotos', archivo_html, width=1000, height=700)
+webview.create_window('Visor de Terremotos', archivo_html, width=1000, height=740)
 webview.start()
 
 fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0}) ##no aseguro
